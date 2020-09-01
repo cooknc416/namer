a4c67d996349b2b992d85ecedb8d5096b2956687
Nathan Cook <cooknc416@gmail.com>
2020-09-01 18:23:39 -0400

    add electron-builder and scripts

---
+++ build-utils/webpack.electron.dev.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+module.exports = {
+  mode: 'development',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  devtool: 'source-map',
+  entry: '../src/electron/main.ts',
+  target: 'electron-main',
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+        ...process.env.stringified,
+        'process.env.FLUENTFFMPEG_COV': false
+    })
+  ]
+};
--- build-utils/webpack.react.prod.config.js
+++ build-utils/webpack.react.prod.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
     mainFields: ['main', 'module', 'browser']
   },
-  entry: './src/react/index.tsx',
+  entry: path.resolve(__dirname, '../src/react/index.tsx'),
   target: 'electron-renderer',
-  devtool: 'source-map',
   module: {
     rules: [
       {
@@ -31,14 +31,6 @@ module.exports = {
         ]
       }
     ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, '../dist/renderer'),
-    historyApiFallback: true,
-    compress: true,
-    hot: true,
-    port: 4000,
-    publicPath: '/'
   },
   output: {
     path: path.resolve(__dirname, '../dist/renderer'),
